a94bbde6bf31b6cb6e940f4367bf9f5614fb7106
Eric Anger <eanger@gmail.com>
2014-10-13 12:45:33 -0400

    Set up logging infrastructure to help debug/analyze

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+#include "../utils/easylogging++.h"
 #include "engine.h"
 
 using namespace std;
 
-int main(){
+_INITIALIZE_EASYLOGGINGPP
+
+int main(int argc, char* argv[]){
+  _START_EASYLOGGINGPP(argc, argv);
   cout << "Hello, world!" << endl;
   eggs::start_engine();
 }
